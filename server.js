934b0ab3f7bb81e42b5003989c9b3b88a56df4c8
krish <krishqatester07@gmail.com>
2022-12-17 22:39:01 -0800

    prep repo for heroku

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ var express = require('express');
 ///Create app Srever
 
 var app = express();
+const PORT = process.evn.PORT || 7070;
+
+app.use(function(req,res,next){
+  if (req.headers['x-forwarded-proto']==='http') {
+    next();
+  }else {
+    res.redirect('http://'+ req.hostname +req.url);
+  }
+});
+
 
 app.use(express.static('public'));
 
-app.listen(7070,function(){
-  console.log("Express server is up on port 7070");
-  })
+app.listen(PORT,function(){
+  console.log("Express server is up on port: "+PORT);
+});
